633960965e4ce51076840f88844450621e1c4a26
Pierre-Jean CHANCELLIER <paidge_cs@hotmail.com>
2023-08-23 20:18:42 +0200

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,8 @@ import fs from "fs-extra"
 export default defineNuxtConfig({
   devServer: {
     https: {
-      key: fs.readFileSync("localhost-key.pem").toString(),
-      cert: fs.readFileSync("localhost.pem").toString()
+      key: fs.readFileSync("../ssl/localhost-key.pem").toString(),
+      cert: fs.readFileSync("../ssl/localhost.pem").toString()
     }
   },
   ssr: false,
